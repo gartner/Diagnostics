60dedaa75d7c04abe84544855cd30cc14e2f0256
Mads Lie Jensen <mads@gartneriet.dk>
2017-08-13 07:49:27 +0200

    Removed usage info for all but diagnostics

--- src/Module.php
+++ src/Module.php
@@ -7,13 +7,11 @@ use Zend\EventManager\EventInterface;
 use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
 use Zend\ModuleManager\Feature\ConsoleBannerProviderInterface;
 use Zend\ModuleManager\Feature\BootstrapListenerInterface;
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 class Module implements
     ConsoleUsageProviderInterface,
-    AutoloaderProviderInterface,
     ConfigProviderInterface,
     ConsoleBannerProviderInterface,
     BootstrapListenerInterface
@@ -35,17 +33,6 @@ class Module implements
         return include __DIR__ . '/../config/module.config.php';
     }
 
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__,
-                ),
-            ),
-        );
-    }
-
     public function getConsoleBanner(ConsoleAdapterInterface $console)
     {
         return self::NAME;
@@ -54,16 +41,12 @@ class Module implements
     public function getConsoleUsage(ConsoleAdapterInterface $console)
     {
         $config = $this->serviceLocator->get('config');
-        if(!empty($config['ZFTool']) && !empty($config['ZFTool']['disable_usage'])){
+        if(!empty($config['ZFTool']) && !empty($config['ZFTool']['disable_usage'])) {
             return null; // usage information has been disabled
         }
 
         // TODO: Load strings from a translation container
         return array(
-
-            'Basic information:',
-            'modules [list]'              => 'show loaded modules',
-            'version | --version'         => 'display current Zend Framework version',
 
             'Diagnostics',
             'diag [options] [module name]'  => 'run diagnostics',
@@ -73,45 +56,6 @@ class Module implements
             array('-q --quiet'                  , 'Do not display any output unless an error occurs.'),
             array('--debug'                     , 'Display raw debug info from tests.'),
 
-            'Application configuration:',
-            'config list'               => 'list all configuration options',
-            'config get <name>'         => 'display a single config value, i.e. "config get db.host"',
-            'config set <name> <value>' => 'set a single config value (use only to change scalar values)',
-
-            'Project creation:',
-            'create project <path>'     => 'create a skeleton application',
-            array('<path>', 'The path of the project to be created'),
-
-            'Module creation:',
-            'create module <name> [<path>]'     => 'create a module',
-            array('<name>', 'The name of the module to be created'),
-            array('<path>', 'The root path of a ZF2 application where to create the module'),
-
-            'Controller creation:',
-            'create controller <name> <module> [<path>]' => 'create a controller in module',
-            array('<name>', 'The name of the controller to be created'),
-            array('<module>', 'The module in which the controller should be created'),
-            array('<path>', 'The root path of a ZF2 application where to create the controller'),
-
-            'Action creation:',
-            'create action <name> <controllerName> <module> [<path>]' => 'create an action in a controller',
-            array('<name>', 'The name of the action to be created'),
-            array('<controllerName>', 'The name of the controller in which the action should be created'),
-            array('<module>', 'The module containing the controller'),
-            array('<path>', 'The root path of a ZF2 application where to create the action'),
-
-            'Classmap generator:',
-            'classmap generate <directory> <classmap file> [--append|-a] [--overwrite|-w]' => '',
-            array('<directory>',        'The directory to scan for PHP classes (use "." to use current directory)'),
-            array('<classmap file>',    'File name for generated class map file  or - for standard output. '.
-                                        'If not supplied, defaults to autoload_classmap.php inside <directory>.'),
-            array('--append | -a',      'Append to classmap file if it exists'),
-            array('--overwrite | -w',   'Whether or not to overwrite existing classmap file'),
-
-            'Zend Framework 2 installation:',
-            'install zf <path> [<version>]' => '',
-            array('<path>', 'The directory where to install the ZF2 library'),
-            array('<version>', 'The version to install, if not specified uses the last available'),
         );
     }
 }
